249a2af61f75f0c9b5b676b2643450356c8d08b0
Singabhattu11Amulya <24wh1a05ce@bvrithyderabad.edu.in>
2026-09-24 11:40:13 +0530

    Add registration and navigation features with custom widgets

---
+++ flutter_application_1/lib/nav.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: FirstScreen(),
+    ),
+  );
+}
+
+class FirstScreen extends StatelessWidget {
+  const FirstScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("First Screen"),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          child: const Text("Go to Second Screen"),
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => const SecondScreen(),
+              ),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
+class SecondScreen extends StatelessWidget {
+  const SecondScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Second Screen"),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          child: const Text("Go Back"),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+      ),
+    );
+  }
+}
